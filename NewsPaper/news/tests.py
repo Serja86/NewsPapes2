cfd45909c36f11a3a393858f404112fe1b342444
Сергей Ловцов <127205415+Serja86@users.noreply.github.com>
2023-07-21 22:07:16 +0300

    Add files via upload

---
+++ NewsPaper/news/tests.py
@@ -0,0 +1,12 @@
+import unittest
+from news.models import *
+
+
+class TestUpdateR(unittest, TestCase):
+    def test_store(self, id=1):
+        a1 = Author.objects.get(id)
+        pR1 = a1.ratingAuthor
+        ps = Post
+        Post.objects.get(id).like()
+
+# Create your tests here.
